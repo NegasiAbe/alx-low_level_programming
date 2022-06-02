833dd24f55c3d003750a01193f2905fb72a2a6c3
NegasiAbe <61984619+NegasiAbe@users.noreply.github.com>
2022-06-02 09:32:07 +0200

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
-# <include stdio.h>
-# <include string.h>
+#include <stdio.h>
+#include <string.h>
  int main (void){
  char str[60];
  strcpy(str , " \"Programming is like building multilingual puzzle");
-
  puts(str);
  return 0;
 
