270d9a7162c4ce4df7c863187af80622cfc8f3d5
Negasi Abebe <negasiabebe@gmail.com>
2022-06-05 10:41:08 -0700

    3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+/**
+ *  main -Prints alphabet in lower case, then in upper case
+ *
+ *  Return 0: sucess 
+ */
+int main(void)
+{
+	char c = 'a';
+	char C = 'A';
+
+	while (c <= z)
+	{
+		putchar(c);
+		c++;
+	}
+	while(C <= Z)
+	{
+		putchar(C);
+		C++;
+	}
+	putchar('\n');
+	return (0);
+}
