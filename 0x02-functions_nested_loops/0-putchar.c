6264a8536f724613daa9c7ee070d8354eff1c105
Negasi Abebe <negasiabebe@gmail.com>
2022-06-07 12:50:03 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,12 +3,11 @@
  * done by NA.
  */
 
-#include<stdio.h>
 #include "main.h"
 
 /**
- * Main -printd putchar
- * Return :0 if success
+ * Main -prints char
+ * Return : Always 0
  */
 int main(void)
 {
