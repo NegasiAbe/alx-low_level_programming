7da67007fab2c963d3496f1dfe598d5fc08d1767
Negasi Abebe <negasiabebe@gmail.com>
2022-06-07 13:50:02 -0700

    1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,14 +2,14 @@
 
  /**
   *
-  * print_alphabet - prints the alphabet in lowercase, followed by a new line.
+  * main -check the code
   * Return: Always 0
   */
- void print_alphabet(void)
+void print_alphabet(void)
 {
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
 		_putchar(letter);
 	_putchar('\n');
- }
+}
