585ec304a2ed83d6f77c40a77efbca610106e33e
NegasiAbe <61984619+NegasiAbe@users.noreply.github.com>
2022-06-07 12:12:06 +0200

    6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,19 +2,18 @@
 
 /**
  * main - Prints the numbers since 0 to 9
- * 
+ *
  * Return: Always (Success)
  */
 int main(void)
 {
-      	int n;
+int n;
 	
-	for (n = '0'; n <= '9'; n++)
-	{
-		putchar(n);
-	}
-       
-	putchar('\n');
+for (n = '0'; n <= '9'; n++)
+{
+putchar(n);
+}
+putchar('\n');
 	
-      	return (0);
+return (0);
 }
