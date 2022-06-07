4b35e307012873ec55cbbb65c8b83bf2c5393d46
NegasiAbe <61984619+NegasiAbe@users.noreply.github.com>
2022-06-07 22:01:59 +0200

    Man

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,27 +1,4 @@
 int _putchar(char c);
-<<<<<<< HEAD
-
-void print_alphabet(void);
-
-void print_alphabet_x10(void);
-
-int _islower(int c);
-
-int _isalpha(int c);
-
-int print_sign(int n);
-
-int _abs(int);
-
-int print_last_digit(int);
-
-void jack_bauer(void);
-
-void times_table(void);
-
-int add(int, int);
-
-=======
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -32,5 +9,4 @@ int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 int add(int, int);
->>>>>>> 78026a09984d1bd47c811b4d5a120436787f38ec
 void print_to_98(int n);
