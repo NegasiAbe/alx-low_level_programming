21bfccaa82c78a588fcb5e89adfc5aab2c32dc2d
Negasi Abebe <negasiabebe@gmail.com>
2022-06-07 15:40:36 -0700

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+/**
+ *
+ * _abs -> return absolute value of a number
+ *
+ * @n: parameter
+ *
+ * Return: absolute value of a number
+ */
+#include "main.h"
+{
+int _abs(int n)
+
+{
+	int n;
+	
+	if (n < 0)
+		n=-1*n;
+	return (n);
+}
