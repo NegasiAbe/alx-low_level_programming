54c11b5235842aa02a5fba97d72708bf1fd819ec
NegasiAbe <61984619+NegasiAbe@users.noreply.github.com>
2022-06-07 21:42:32 +0200

    0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,11 +7,12 @@
 #include "main.h"
 
 /**
- * Main -printd putchar
+ * Main -print a characters of putchar after using user defined language
  * Return :0 if success
  */
 int main(void)
 {
+	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
