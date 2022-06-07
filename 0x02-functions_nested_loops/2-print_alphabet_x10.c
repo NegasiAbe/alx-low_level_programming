4203badf9426e84a4c526986b126b04acefde059
NegasiAbe <61984619+NegasiAbe@users.noreply.github.com>
2022-06-07 23:50:01 +0200

    2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,13 +5,13 @@
 void print_alphabet_x10(void)
 {
 	int i;
-	int j=10;
-	
-	while(j !=0 )
+	int j = 10;
+
+	while (j ! =0)
 	{
-		for(i ='a'; i <= 'z'; i++)
+		for (i = 'a'; i <= 'z'; i++)
 			_putchar(i);
 		_putchar('\n');
-		j--;
+	j--;
 	}	
 }
