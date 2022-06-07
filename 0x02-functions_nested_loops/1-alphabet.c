edf285d392e75509bc96a003569e87341a62aec8
NegasiAbe <61984619+NegasiAbe@users.noreply.github.com>
2022-06-07 14:30:16 +0200

    1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,10 @@
-uth: Brennan D Baraban
-
- */
-
-
-
-#include "holberton.h"
-
-
+#include "main.h"
 
  /**
   *
-  *  * print_alphabet - prints the alphabet in lowercase, followed by a new line.
+  * print_alphabet - prints the alphabet in lowercase, followed by a new line.
   *
-  *   */
+  */
 
  void print_alphabet(void)
 
@@ -20,14 +12,7 @@ uth: Brennan D Baraban
 
 		char letter;
 
-
-
-			for (letter = 'a'; letter <= 'z'; letter++)
-
-						_putchar(letter);
-
-
-
-				_putchar('\n');
-
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+		_putchar('\n');
 }
