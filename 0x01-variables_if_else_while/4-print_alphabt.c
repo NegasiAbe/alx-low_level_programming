6006afcb18aa37f9505a567d5e4514c25fd5f349
NegasiAbe <61984619+NegasiAbe@users.noreply.github.com>
2022-06-07 09:36:58 +0200

    4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,26 +1,22 @@
-/*
-* File: 4-print_alphabt.c
-* Auth: Brennan D Baraban
-*/
-
 #include <stdio.h>
 
 /**
-* main - Prints the alphabet in lowercase, except for q and e.
-*
-* Return: Always 0.
-*/
+ *main -> assign a random number to the variable n each time it is executed
+ *and print the last digit of the number stored in the variable n
+ *Return: Always 0 (Success)
+ */
 int main(void)
 {
-	char letter;
-	
-	for (letter = 'a'; letter <= 'z'; letter++)
-	{
-		if (letter != 'e' && letter != 'q')
-			putchar(letter);
-	}
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+if (ch != 'q' && ch != 'e')
+{
+putchar(ch);
+}
+}
 
-	putchar('\n');
+putchar(10); /* this is an ascii code for new line*/
 
-	return (0);
+return (0);
 }
