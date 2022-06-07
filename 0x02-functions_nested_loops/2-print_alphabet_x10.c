d4fb3b8161f7f0f2fbf9734713b0032c5318f2fc
NegasiAbe <61984619+NegasiAbe@users.noreply.github.com>
2022-06-07 23:40:20 +0200

    2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,15 +2,16 @@
  * print_alphabet_x10 -prints lower case letters10 times
  */
 #include "main.h"
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
-	int i,j=10;
+	int i;
+	int j=10;
+	
 	while(j !=0 )
 	{
 		for(i ='a'; i <= 'z' i++)
 			_putchar(i);
 		_putchar('\n')
 		j--;
-	}
-	
+	}	
 }
