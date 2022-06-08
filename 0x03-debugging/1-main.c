4cc43a20d2285a66142c8334da833633da1cc7bc
NegasiAbe <61984619+NegasiAbe@users.noreply.github.com>
2022-06-08 12:59:00 +0200

    Commenting

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -14,7 +14,6 @@ int main(void)
 
 	printf("Infinite loop incoming :(\n");
 
-
  	i = 0;
 
 	/**
@@ -23,7 +22,7 @@ int main(void)
          * putchar(i);
 	 }*
 	 */
+	
 	printf("Infinite loop avoided! \\o/\n");
-
 	return (0);
 }
