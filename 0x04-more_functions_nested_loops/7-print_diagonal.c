be74395698145d6565b14e8f9524bc2f1482aaf3
Negasi Abebe <negasiabebe@gmail.com>
2022-06-09 15:53:06 -0700

    diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_diagonal -prints a backslash in diagonal 
+ */
+void print_diagonal(void)
+{
+	int i, j;
+
+	if (i <= 0)
+		_putchar('\n');
+	else
+	{
+		for (j = 0; j < i; j++)
+			for (j = 0; j < i; j++)
+			
+			{	
+				_putchar(' ');
+			_putchar(92);
+			_putchar('\n');
+			}
+	}
+}
