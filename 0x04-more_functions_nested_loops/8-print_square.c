268d4121ee8c110171566694617aa3e0fba32373
Negasi Abebe <negasiabebe@gmail.com>
2022-06-09 16:33:05 -0700

    print square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_square -prints squre hash
+ * @size: size argument
+ */
+void print_diagonal(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for(i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+				_putchar(35);
+			_putchar('\n');
+		}
+	}
+}
