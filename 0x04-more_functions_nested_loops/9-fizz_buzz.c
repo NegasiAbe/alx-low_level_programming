2abc0fb0f60bcc30daa4573d6582cde8df0b40ab
Negasi Abebe <negasiabebe@gmail.com>
2022-06-09 17:03:14 -0700

    fizzduzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,13 @@
+#include "main.h"'
+#include<stdio.h>
+
+/**
+ * Main -prints numbers 1 to 100 followed by newline
+ * but it prints fizz the number is multiple of 3
+ * It prints Dizz if the nuber is multiple of 5
+ * prints fizzDuzz if it is multipleof 3 and 5
+ */
+void main(void)
+{
+}
+
