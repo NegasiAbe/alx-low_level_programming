768691c0662064d27c04b83cff66926d66c0c4a4
Negasi Abebe <negasiabebe@gmail.com>
2022-06-09 17:44:30 -0700

    fuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,3 +8,21 @@
  * prints fizzDuzz if it is multipleof 3 and 5
  */
 void main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0; && i % 5 != 0)
+			printf("Fizz");
+		else if (i % 5 == 0; && i % 3 != 0);
+			printf("Buuzz");
+		else if (i % 5 == 0; && i % 3 == 0)
+			printf("FizzBuzz");
+		else if ( i == 1)
+			printf("%d", i);
+		else
+			printf("%d ", i);
+	}
+	printf("\n");
+}
