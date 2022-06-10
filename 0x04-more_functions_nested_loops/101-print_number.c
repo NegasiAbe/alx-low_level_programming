1089c8f8ef7e381a5d0b0cb91cafae6ef3d0b91f
NegasiAbe <61984619+NegasiAbe@users.noreply.github.com>
2022-06-10 04:32:48 +0200

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -12,15 +12,11 @@ void print_number(int n)
 	{
 		i = -n;
 		_putchar('-');
-	} else
-	{
-		i = n;
-	}
-
+	} 
 	if ((i / 10) > 0)
 	{
 		print_number(i / 10);
 	}
 
-	_putchar(i / 10);
+	_putchar((i % 10) + '0');
 }
