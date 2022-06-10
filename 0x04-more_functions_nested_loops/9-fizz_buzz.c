296f2faf24759ce2a85e6cb31ac3be31871de2b4
NegasiAbe <61984619+NegasiAbe@users.noreply.github.com>
2022-06-10 02:51:37 +0200

    Fizz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,13 +13,13 @@ void main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0; && i % 5 != 0)
+		if (i % 3 == 0; &&i % 5 != 0)
 			printf("Fizz");
-		else if (i % 5 == 0; && i % 3 != 0);
+		else if (i % 5 == 0 &&i % 3 != 0);
 			printf("Buuzz");
-		else if (i % 5 == 0; && i % 3 == 0)
+		else if (i % 5 == 0 &&i % 3 == 0)
 			printf("FizzBuzz");
-		else if ( i == 1)
+		else if (i == 1)
 			printf("%d", i);
 		else
 			printf("%d ", i);
