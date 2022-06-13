76f011e5142663f50dfb00aef938373d20b770ba
Negasi Abebe <negasiabebe@gmail.com>
2022-06-13 08:21:10 -0700

    string commiting

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _puts -prints the string characte
+ * @s: charcter to be printed
+ * Return: no value
+ */
+void _puts(char *str)
+{
+	for (str = 0; *str != '\0'; str++)
+		_putchar(str);
+	_putchar('\n');
+}
