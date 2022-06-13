861c33a0b49339faecb3ba7cb25642b8f453a3e4
Negasi Abebe <negasiabebe@gmail.com>
2022-06-13 06:50:03 -0700

    swapping

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int -swaps values of two integers by swapingers by swaping thier address
+ * @a: first integer to be swaped
+ * @b: second integer to be swaped
+ * Return: no value
+ */
+void swap_int(int *a, int*b)
+{
+	int temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
