9fcfae7b5995f6e24d3503efa9c52365540319c3
NegasiAbe <61984619+NegasiAbe@users.noreply.github.com>
2022-06-13 22:17:36 +0200

    Reversal

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -4,14 +4,13 @@
 * print_rev -> printing a string in reverse
 * @s: the string to be printed in rev
 */
-
 void print_rev(char *s)
 {
 	int i, j;
 
 	j = 0;
 	while (s[j] != '\0')
-		n++;
+		j++;
 
 	for (i = j - 1; i >= 0; i--)
 	{
