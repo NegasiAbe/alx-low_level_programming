2157d602041538218e98647fa28757934fcdc3c8
NegasiAbe <61984619+NegasiAbe@users.noreply.github.com>
2022-06-13 22:13:45 +0200

    string_reversal

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,25 +1,21 @@
-#include "holberton.h"
-#include <stdio.h>
+#include "main.h"
 
 /**
-  * print_rev - Prints a string in reverse
-  * @s: The string to print
-  *
-  * Return: void
-  */
+* print_rev -> printing a string in reverse
+* @s: the string to be printed in rev
+*/
+
 void print_rev(char *s)
 {
-	int c = 0;
+	int i, j;
 
-	while (s[c] != '\0')
+	j = 0;
+	while (s[j] != '\0')
+		n++;
+
+	for (i = j - 1; i >= 0; i--)
 	{
-		c++;
+		_putchar(s[i]);
 	}
-
-	for (c -= 1; c >= 0; c--)
-	{
-		_putchar(s[c]);
-	}
-
 	_putchar('\n');
 }
