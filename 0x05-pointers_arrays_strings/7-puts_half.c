20572cf6c0ce1bd854de5fe5b897aa613982147e
NegasiAbe <61984619+NegasiAbe@users.noreply.github.com>
2022-06-14 00:17:24 +0200

    prints the upper half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,2 +1,20 @@
 #include "main.h"
+#include <string.h>
 
+/**
+* puts_half -> puts half
+* @str: string param
+*/
+void puts_half(char *str)
+{
+	int i, j, k;
+
+	i = strlen(str);
+	if (i % 2 == 1)
+		j = i / 2 + 1;
+	else
+		j = i / 2;
+	for (k = j; k < i; k++)
+		_putchar(str[k]);
+	_putchar('\n');
+}
