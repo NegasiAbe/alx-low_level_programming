d4d8bed039f5abb95e759bfdb66ee4221e7db521
NegasiAbe <61984619+NegasiAbe@users.noreply.github.com>
2022-06-13 18:26:30 +0200

    reverse printing

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,11 +1,25 @@
-#include "main.c"
+#include "holberton.h"
+#include <stdio.h>
 
 /**
- * print_rev -printsa string in reverse order
- * @s: string to be printed
- * Return: no value
- */
-void print_rev (char *s)
+  * print_rev - Prints a string in reverse
+  * @s: The string to print
+  *
+  * Return: void
+  */
+void print_rev(char *s)
 {
+	int c = 0;
 
+	while (s[c] != '\0')
+	{
+		c++;
+	}
+
+	for (c -= 1; c >= 0; c--)
+	{
+		_putchar(s[c]);
+	}
+
+	_putchar('\n');
 }
