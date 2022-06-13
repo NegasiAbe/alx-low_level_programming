73a2323694ebdec4982a7f50e63cf0288a40b428
NegasiAbe <61984619+NegasiAbe@users.noreply.github.com>
2022-06-13 17:56:09 +0200

    putchar

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
 /**
- * _puts -prints the string characte
- * @str: charcter to be printed
- * Return: no value
- */
+  * _puts - Prints a string
+  * @str: The string to print
+  *
+  * Return: void
+  */
 void _puts(char *str)
 {
 	for (; *str != '\0'; str++)
 	{
-		_putchar(str);
+		_putchar(*str);
 	}
 
 	_putchar('\n');
