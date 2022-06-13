605e557741a169a415c5e0b4599c136ed20f2788
NegasiAbe <61984619+NegasiAbe@users.noreply.github.com>
2022-06-13 23:58:17 +0200

    length_by_string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,2 +1,16 @@
 #include "main.h"
+#include <string.h>
 
+/**
+* puts2 -> puts2 function
+* @str: str parameter
+*/
+void puts2(char *str)
+{
+	int length, i;
+
+	length = strlen(str);
+	for (i = 0; i < length; i += 2)
+		_putchar(str[i]);
+	_putchar('\n');
+}
