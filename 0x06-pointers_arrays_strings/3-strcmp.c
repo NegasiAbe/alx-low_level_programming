7d8772046dee4f484d6ef7ccb8c0dc4d2e3cba40
NegasiAbe <61984619+NegasiAbe@users.noreply.github.com>
2022-06-15 18:26:11 +0200

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,6 +8,43 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0, j = 0, k = 0;
+	int i = 0, j = 0, k = 0, r=0, lim;
+	while (s1[i])
+	{
+		i++;
+	}
+
+	while (s2[j])
+	{
+		j++;
+	}
+
+	if (i <= j)
+	{
+		lim = i;
+	}
+	else
+	{
+		lim = j;
+	}
+
+	while (k <= lim)
+	{
+		if (s1[c] == s2[c])
+		{
+			c++;
+			continue;
+		}
+		else
+		{
+			r = s1[c] - s2[c];
+			break;
+		}
+
+		c++;
+	}
+
+	return (r);
+}
 
 }
