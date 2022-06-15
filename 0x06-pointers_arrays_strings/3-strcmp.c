e407d60440d4b74ee159cc954f6ecc6bb1993910
Negasi Abebe <negasiabebe@gmail.com>
2022-06-15 09:18:52 -0700

    comparison

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strcmp -compares two strings
+ * @s1:first string
+ * @s2:second string
+ * Return:comparison result
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0, j = 0, k = 0;
+
+}
