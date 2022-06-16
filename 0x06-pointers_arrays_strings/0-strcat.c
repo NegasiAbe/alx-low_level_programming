46398fea7de1284a894ab6393c6cb352fb293eee
NegasiAbe <61984619+NegasiAbe@users.noreply.github.com>
2022-06-16 11:41:25 +0200

    String copy

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,3 +1,26 @@
 #include "main.h"
 
+/**
+ * _strcat -concatnates the two strins needed
+ * @dest:destination string value
+ * @src:src string value
+ * Return: concatnated string.
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0, j;
 
+	while (dest[i])
+	{
+		i++;
+	}
+
+	for (j = 0; src[j] != '\0'; j++)
+	{
+		dest[i] = src[j];
+		i++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
