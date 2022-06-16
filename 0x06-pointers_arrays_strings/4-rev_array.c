b2be6295c892bb367f8b0ab314d14c25a165f190
NegasiAbe <61984619+NegasiAbe@users.noreply.github.com>
2022-06-16 12:43:25 +0200

    Array_reversion

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,1 +1,22 @@
-#
+#include "main.h"
+
+/**
+* reverse_array -> reversing an array
+* @a: array a
+* @n: an element of an array
+*/
+void reverse_array(int *a, int n)
+{
+	int *p, i, temp, j;
+
+	p = a;
+	for (i = 0; i < n; i++)
+		p++;
+	for (k = 0; j < i / 2; j++)
+	{
+		aux = a[j];
+		a[j] = *p;
+		*p = temp;
+		p--;
+	}
+}
