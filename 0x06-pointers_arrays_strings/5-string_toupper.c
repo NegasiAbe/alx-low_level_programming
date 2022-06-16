897e19769565f52702896ec00061f46e67cc5200
NegasiAbe <61984619+NegasiAbe@users.noreply.github.com>
2022-06-16 13:20:29 +0200

    Change to upper case

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,3 +1,18 @@
 #include "main.h"
 /**
- *
+* string_toupper - converting any string to upper case 
+* @x: string parameter to be converted
+* Return:  upper case string 
+*/
+char *string_toupper(char *x)
+{
+	int i = 0;
+
+	while (x[i])
+	{
+		if (x[i] >= 97 && x[i] <= 122)
+			x[i] = x[i] - 32;
+		i++;
+	}
+	return (x);
+}
