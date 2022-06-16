89026910db9493a15c4d4036d616e18046734625
NegasiAbe <61984619+NegasiAbe@users.noreply.github.com>
2022-06-16 12:10:28 +0200

    String copy with length limitation

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strncpy -copies string of length n.
+ *  @dest: destinatin string
+ *  @src:source string
+ *  @n:length of string to be coppied
+ * Return:copied string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j = 0;
+	
+
+	while (j < n && src[j])
+	{
+	dest[j] = src[j];
+		j++;
+	}
+	while (j < n)
+	{
+	dest[j] = '\0';
+		j++;
+	}
+	return (dest);
+}
