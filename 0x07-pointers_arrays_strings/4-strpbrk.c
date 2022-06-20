7563f0656602e93ba4285250b7dec3cbdd0713b6
NegasiAbe <61984619+NegasiAbe@users.noreply.github.com>
2022-06-20 14:53:44 +0200

    String searcher

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -8,5 +8,18 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-	
+	int i;
+
+	while (*s)
+	{
+		for (index = 0; accept[i]; i++)
+		{
+			if (s[i] == accept[i])
+				return (s);
+		}
+
+		s++;
+	}
+
+	return ('\0');
 }
