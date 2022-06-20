e9e8285a24f900c8179f7671edc13706bd309c32
Negasi Abebe <negasiabebe@gmail.com>
2022-06-20 07:46:14 -0700

    substring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _strstr -substring locator
+ * @haystack: a string to be checked
+ * @needle:string to be searched
+ * Return:string
+ */
+char *_strstr(char *haystack, char *needle)
+{
+
+}
