fe105511de20988221419a45ecb4e5022d3b92f4
NegasiAbe <61984619+NegasiAbe@users.noreply.github.com>
2022-06-20 17:49:25 +0200

    Diagonal matrix

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -2,5 +2,26 @@
 
 /**
  * print_diagsums -> prints 
+ * @a: matrix to be printed
+ * @size: size of the matrix
  */
 void print_diagsums(int *a, int size)
+{
+	int i, sum1 = 0, sum2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		sum1 += a[i];
+		a += size;
+	}
+
+	a -= size;
+
+	for (index = 0; i < size; i++)
+	{
+		sum2 += a[i];
+		a -= size;
+	}
+
+	printf("%d, %d\n", sum1, sum2);
+}
