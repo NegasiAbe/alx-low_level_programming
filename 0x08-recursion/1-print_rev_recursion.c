b913c3face75ff4ed2ee450ecb4ffada6df8f707
NegasiAbe <61984619+NegasiAbe@users.noreply.github.com>
2022-06-22 16:43:15 +0200

    reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,18 +8,11 @@
  */
 void _print_rev_recursion(char *s)
 {
-	int len;
-
-	while(*s)
+	if (*s != '\0')
 	{
-		len++;
-	s++;
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
 	}
-	while (*s)
-	{
-		_putchar(*(s - 1));
-	s--;
-	}
-	_putchar('\n');
-
+	else
+		return;
 }
