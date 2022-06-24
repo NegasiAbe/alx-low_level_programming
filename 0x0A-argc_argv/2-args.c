3dd8b81cc9f9b05ce966cb44c166dccefbf5f71f
NegasiAbe <61984619+NegasiAbe@users.noreply.github.com>
2022-06-25 00:37:20 +0200

    prints all arguments it receives

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,1 +1,17 @@
 #include<stdio.h>
+/**
+* main -> this is a function to print its name
+* @argc: argc parameter
+* @argv: an array of a command listed
+* Return: 0 for success
+*/
+int main(int argc, char *argv[])
+{
+	int j;
+
+	for (j = 0; j < argc; j++)
+	{
+		printf("%s\n", argv[j]);
+	}
+	return (0);
+}
