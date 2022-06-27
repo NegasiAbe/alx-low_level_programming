9d95becb54ec6f50603f6f418ccc04c261e1a485
NegasiAbe <61984619+NegasiAbe@users.noreply.github.com>
2022-06-27 17:23:54 +0200

    new string allocation

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -8,5 +8,28 @@
  */
 char *_strdup(char *str)
 {
+int i = 0, j = 1;
+	char *s;
 
+	if (str == NULL)
+		return (NULL);
+
+	while (str[j])
+	{
+		i++;
+	}
+
+	s = malloc((sizeof(char) * j) + 1);
+
+	if (s == NULL)
+		return (NULL);
+
+	while (i < j)
+	{
+		s[i] = str[i];
+		i++;
+	}
+
+	s[i] = '\0';
+	return (s);
 }
