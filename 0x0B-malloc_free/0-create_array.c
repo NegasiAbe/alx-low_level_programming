7ec7cf3221e42304060d09d7c93b72e36ff10b8d
NegasiAbe <61984619+NegasiAbe@users.noreply.github.com>
2022-06-27 17:09:08 +0200

    character_array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -7,3 +7,22 @@
  * Return: return an array char
  */
 char *create_array(unsigned int size, char c)
+  {
+	unsigned int index;
+	char *s;
+
+	if (size == 0)
+		return (NULL);
+
+	s = malloc(size * sizeof(char));
+
+	if (s == NULL)
+		return (NULL);
+
+	for (index = 0; index < size; index++)
+	{
+		s[index] = c;
+	}
+
+	return (s);
+}
