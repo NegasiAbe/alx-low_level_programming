ef2114fb334f34e7a10a7965eced6f4b08e735e6
Negasi Abebe <negasiabebe@gmail.com>
2022-06-27 08:21:35 -0700

    string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include<stdlib.h>
+
+/**
+ * _strdup ->finds a newly allocated space
+ * @str: string passed
+ * Return: returns a charcter array
+ */
+char *_strdup(char *str)
+{
+
+}
