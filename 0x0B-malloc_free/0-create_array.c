230b87de82b35d452f55e5468dcd342b6f6c590f
Negasi Abebe <negasiabebe@gmail.com>
2022-06-27 08:06:44 -0700

    character array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,9 @@
+#include "main.h"
+#include<stdlib.h>
+/**
+ * create_array ->creates size c
+ * @size: size of thearray
+ * @c: character argument
+ * Return: return an array char
+ */
+char *create_array(unsigned int size, char c)
---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,11 @@
+#include"main.h"
+#include<stdlib.h>
+
+/**
+ * free_grd ->frees up two dimensional greed
+ * @grid: address of two dimensional array
+ * @height: height of the address
+ * Returns:  no value
+ */
+void free_grid(int **grid, int height)
+
