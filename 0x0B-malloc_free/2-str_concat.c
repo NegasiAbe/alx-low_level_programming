f1f3a3ce4f8470c66c118bebfb78bf07a9826e1b
NegasiAbe <61984619+NegasiAbe@users.noreply.github.com>
2022-06-27 18:03:21 +0200

    String_concatination

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -4,8 +4,8 @@
 
 /**
  * str_concat -> concatinates two sstrings
- * @s1: string one too be concatenated 
- * @s2: string two to be concated  
+ * @s1: string one too be concatenated
+ * @s2: string two to be concated
  */
 char *str_concat(char *s1, char *s2)
 {
