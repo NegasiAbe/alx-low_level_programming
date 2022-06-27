e8c15c5fb1fe53b986632f042484046806e7a962
Negasi Abebe <negasiabebe@gmail.com>
2022-06-27 09:14:49 -0700

    two dimensional array

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,9 @@
+#include "main.h"
+#include<stdlib.h>
+
+/**
+ * alloc_grid -> creates two dimensional array integers
+ * @width: width of the array
+ * @height: height od the array
+ */
+int **alloc_grid(int width, int height)
