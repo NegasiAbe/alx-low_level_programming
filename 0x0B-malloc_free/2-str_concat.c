b24c3b49eb9e42e4f10a3fc90b79b442a0298716
Negasi Abebe <negasiabebe@gmail.com>
2022-06-27 08:38:13 -0700

    concatnates two strings

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include<stdlib.h>
+
+/**
+ * str_concat -> concatinates two sstrings
+ * @s1: string one too be concatenated 
+ * @s2: string two to be concated  
+ */
+char *str_concat(char *s1, char *s2)
+{
+
+}
