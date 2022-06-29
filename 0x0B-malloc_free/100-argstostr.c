3b226c15b962b787525ae1e203db94a056fce6c3
Negasi Abebe <negasiabebe@gmail.com>
2022-06-29 15:24:53 -0700

    concatnation

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+/**
+ * argstostr -concatnates argument of my type
+ * @ac: integer argument
+ * @av: character argument
+ * Return: concatnated string
+ */
+char *argstostr(int ac, char **av)
