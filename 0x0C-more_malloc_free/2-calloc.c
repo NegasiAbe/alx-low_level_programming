03f1c94b84d771e7eb08e7139bdfc4e4eec1d86e
NegasiAbe <61984619+NegasiAbe@users.noreply.github.com>
2022-06-30 19:06:17 +0200

    Memory_allaction

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -8,5 +8,23 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+int i = 0, len = 0;
+	char *p;
 
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	len = nmemb * size;
+	p = malloc(len);
+
+	if (p == NULL)
+		return (NULL);
+
+	while (i < l)
+	{
+		p[i] = 0;
+		i++;
+	}
+
+	return (p);
 }
