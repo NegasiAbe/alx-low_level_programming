4dc12542088c96ee6293c3bca07c3413bd032312
NegasiAbe <61984619+NegasiAbe@users.noreply.github.com>
2022-06-30 19:08:05 +0200

    Min_Max

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -9,5 +9,22 @@
  */
 int *array_range(int min, int max)
 {
+int *p, i = 0;
 
+	if (min > max)
+		return (NULL);
+
+	p = malloc((sizeof(int) * (max - min)) + sizeof(int));
+
+	if (p == NULL)
+		return (NULL);
+
+	while (min <= max)
+	{
+		p[i] = min;
+		i++;
+		min++;
+	}
+
+	return (p);
 }
