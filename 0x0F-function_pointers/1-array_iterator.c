921d88a83e934103a7884e28ad0fdddc4882fb0e
Negasi Abebe <negasiabebe@gmail.com>
2022-07-06 10:03:55 -0700

    function pointer

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator -> prints each element of array
+ * @array: array to be iterated
+ * @size: size of the array
+ * @action: pointer of a function
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+
+}
