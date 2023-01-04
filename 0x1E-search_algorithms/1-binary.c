3ede878f2601ce7a38f2ba25c9e1f89264271829
NegasiAbe <61984619+NegasiAbe@users.noreply.github.com>
2023-01-04 23:49:37 +0100

    solution updated

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,45 +1,31 @@
 #include "search_algos.h"
 
 /**
- * binary_search - searches for a value in an integer array using a binary
- * search algorithm, not guaranteed to return lowest index if `value` appears
- * twice in `array`
- * @array: pointer to first element of array to seach
- * @size: number of elements in array
- * @value: value to search for
+ * linear_search - searches for a value in an array of
+ * integers using the Linear search algorithm
  *
- * Return: index containing `value`, or -1 if `value` not found or
- * `array` is NULL
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search in
+ * Return: Always EXIT_SUCCESS
  */
 
-int binary_search(int *array, size_t size, int value)
+int linear_search(int *array, size_t size, int value)
 {
-	int low, mid, high;
-	int x;
+	size_t x;
 
 	if (array == NULL)
 	{
 		return (-1);
 	}
 
-	low = 0;
-	high = size - 1;
-
-	while (low <= high)
+	for (x = 0; x < size; x++)
 	{
-		mid = (low + high) / 2;
-
-		printf("Searching in array: ");
-		for (x = low; x <= high; x++)
-			printf("%i%s", array[x], x == high ? "\n" : ", ");
-
-		if (array[mid] < value)
-			low = mid + 1;
-		else if (array[mid] > value)
-			high = mid - 1;
-		else
-			return (mid);
+		printf("Value checked array[%li] = [%i]\n", x, array[x]);
+		if (array[x] == value)
+		{
+			return (x);
+		}
 	}
-
 	return (-1);
 }
